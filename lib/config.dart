8c18e1bb7135d4ba6a4843b644fe6ea59e2704db
iMAC <treyrosius@gmail.com>
2021-04-13 22:13:48 +0100

    more updates

--- lib/config.dart
+++ lib/config.dart
@@ -1,3 +1,4 @@
 class Config{
-  static final String API_KEY="8ZvY9h9IVx4r7501GZ5Ep6DNOFih5Hkr7183q7br";
+  //REPLACE WITH YOUR OWN KEY
+  static final String API_KEY="XXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 }
